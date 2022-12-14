0c96fa3a1188fb0d898c82bc35378a01c2e4b9a3
mlamamra <mlamamra@dawan.fr>
2022-12-14 15:59:40 +0100

    génération et prélèvement du rapport de test unitaires

--- cicd_angular/karma.conf.js
+++ cicd_angular/karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function (config) {
         require('karma-jasmine'),
         require('karma-chrome-launcher'),
         require('karma-jasmine-html-reporter'),
+        require('karma-junit-reporter'),
         require('karma-coverage'),
         require('@angular-devkit/build-angular/plugins/karma'),
         ],
@@ -29,7 +30,11 @@ module.exports = function (config) {
             { type: 'text-summary' }
         ]
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'junit'],
+        junitReporter: {
+            outputDir: 'karma-results',
+            outputFile: 'karma-results.xml'
+        },
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
